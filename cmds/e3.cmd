1afa3de91358a33530e017f8384bcbd798720b3b
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-02-09 13:58:00 +0100

    add turnoff script, e3 with autosave

--- cmds/e3.cmd
+++ cmds/e3.cmd
@@ -1,4 +1,5 @@
-require calc, 3.7.1
+#require calc, 3.7.1
+require autosave,5.9.0
 
 epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
 epicsEnvSet("ENGINEER","hanlee x3409")
@@ -10,7 +11,8 @@ epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(DB_TOP)")
 
 dbLoadTemplate("$(DB_TOP)/Bingo1-90.substitutions","P=Bingo90:")
 
-iocInit
+iocshLoad("$(autosave_DIR)/autosave.iocsh", "IOCNAME=$(IOCNAME), AS_TOP=$(TOP)")
 
+iocInit
 
 dbl > "$(TOP)/$(IOCNAME)_PVs.list"
